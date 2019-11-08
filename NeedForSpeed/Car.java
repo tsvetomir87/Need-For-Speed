3c3889c1e89506223122c2a661205aa0340bcb42
Tsvetomir Nachev <57493062+tsvetomir87@users.noreply.github.com>
2019-11-08 19:42:48 +0200

    Add files via upload

---
+++ NeedForSpeed/Car.java
@@ -0,0 +1,9 @@
+package NeedForSpeed;
+
+public class Car extends Vehicle{
+
+    public Car(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        this.setFuelConsumption(3);
+    }
+}
